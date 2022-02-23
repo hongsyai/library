52872b66bcfadde72e1ed8365da3276d2fdfffbf
Hong <hongsyai@gmail.com>
2022-02-23 10:28:08 +0900

    이건일단 취소

--- src/main/java/org/smart/board/controller/BookController.java
+++ src/main/java/org/smart/board/controller/BookController.java
@@ -38,4 +38,34 @@ public class BookController {
         return "book/bookList";
     }
 
+    //    @GetMapping("/bookDetail")
+//    public String bookDetail() {
+//
+//        return "book/bookDetail";
+//    }
+    /**
+     * 글 자세히 보기 화면 요청
+     * @param bookseq
+     * @param model
+     * @return
+     */
+    @GetMapping("/bookDetail")
+    public String bookDetail(Long bookseq, Model model) {
+        // 1) DB에서 boardseq에 해당하는 하나의 글을 질의해옴
+        // 1-1) 조회수 증가해야함
+        Book book  = bookService.findOne(bookseq);
+
+        // 2) Model에 저장
+        model.addAttribute("book", book);
+
+        // 3) view로 forward
+        return "book/bookDetail";
+    }
+
+    @GetMapping("/bookSearch")
+    public String bookSearch() {
+
+        return "book/bookSearch";
+    }
+
 }
